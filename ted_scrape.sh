3c9fb891a707ca670e93aae6444ae197e4b51244
Salma Elshahawy <salmaeng71@gmail.com>
2020-12-14 18:08:47 -0500

    add bash script

--- ted_scrape.sh
+++ ted_scrape.sh
@@ -3,20 +3,20 @@
 for i in {1..107}
 do
     curl "https://www.ted.com/talks?sort=popular&language=en&topics%5B%5D=Technology&page=$i&sort=popular" |
-    grep "href='/talks/" |
-    sed "s/^.*href='/https\:\/\/www\.ted\.com/" |
-    sed "s/?.*$/\/transcript/" |
-    uniq
-done > ted_links.csv
+    grep "href='/talks/" | # grap only talks that have a link
+    sed "s/^.*href='/https\:\/\/www\.ted\.com/" | 
+    sed "s/?.*$/\/transcript/" | # clean the link to fetch the transcript only
+    uniq # remove duplictae
+done > ted_links.csv # save the links in a csv file
 
 counter=1
 for i in $(cat ted_links.csv)
 do
     curl $i |
     html2text |
-    sed -n '/Details About the talk/,$p' |
-    sed -n '/Programs &amp. initiatives/q;p' |
-    head -n-1 |
-    tail -n+2 > ted_transcripts/talk$counter.csv
+    sed -n '/Details About the talk/,$p' | # remove extra space at the top of each talk
+    sed -n '/Programs &amp. initiatives/q;p' | # remove footer from **** Programs & initiatives ****
+    head -n-1 |  # remove header space
+    tail -n+2 > ted_transcripts/talk$counter.csv # remove footer and seperate each talk into a seperate file
     counter=$((counter+1))
 done
